3a684f1c556f33bcf48f4d2d2954dab11e29692d
Karlo Marunić <karlo.marunic@gmail.com>
2022-03-21 22:30:41 +0100

    Add project files.

---
+++ EURIS.Service/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EURIS.Entities;
+
+namespace EURIS.Service
+{
+    public class ProductManager
+    {
+        readonly LocalDbEntities _context = new LocalDbEntities();
+
+        public List<Product> GetProducts()
+        {
+            var products = (from item in _context.Product select item).ToList();
+            return products;
+        }
+    }
+}
---
+++ EurisTest.Web/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using EURIS.Service;
+
+namespace EurisTest.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            var productManager = new ProductManager();
+            var products = productManager.GetProducts();
+            ViewBag.Products = products;
+            return View();
+        }
+    }
+}
